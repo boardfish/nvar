403c56220b56247db72058435ae3a630442586b6
Simon Fish <si@mon.fish>
2022-01-06 14:21:58 +0000

    Spread slice args
    Fixes issues on Ruby 3

--- lib/nvar/environment_variable.rb
+++ lib/nvar/environment_variable.rb
@@ -16,7 +16,7 @@ module Nvar
       @type = type
       @required = args[:required].nil? ? true : args[:required]
       @filter_from_requests = filter_from_requests.yield_self { |f| [true, false].include?(f) ? f : f&.to_sym }
-      @value = fetch_value(args.slice(:passthrough, :default_value))
+      @value = fetch_value(**args.slice(:passthrough, :default_value))
       @defined = true
     rescue KeyError
       @value = args[:default_value]
